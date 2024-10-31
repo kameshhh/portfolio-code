396881ff32b5390667e71cd684ff52994fa42be7
Kamesh Kumar <kameshkumar4602@gmail.com>
2024-10-31 21:19:06 +0530

    Bug: Fixed the dark mode button functionality

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ function toggleMenu(){
 }
 
 var viewIcon = document.getElementById("view-icon");
+var viewIcon1 = document.getElementById("view-icon-1");
 
 viewIcon.onclick = function(){
+    console.log("happy")
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
         viewIcon.setAttribute("src", "./assets/sun.png");
@@ -15,5 +17,16 @@ viewIcon.onclick = function(){
         viewIcon.setAttribute("src", "./assets/moon.png");
     }
 }
+
+viewIcon1.onclick = function(){
+    console.log("happy")
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        viewIcon1.setAttribute("src", "./assets/sun.png");
+    }else{
+        viewIcon1.setAttribute("src", "./assets/moon.png");
+    }
+}
+
 
  
