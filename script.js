46bcf3fac1ef76d8ddeea0c485bc4c309256c6ac
Kamesh Kumar <kameshkumar4602@gmail.com>
2024-10-31 23:40:43 +0530

    updated the javascript to be more efficient

--- script.js
+++ script.js
@@ -2,61 +2,37 @@ function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
-    icon.classList.toggle("open")
+    icon.classList.toggle("open");
 }
 
-var viewIcon = document.getElementById("view-icon");
-var viewIcon1 = document.getElementById("view-icon-1");
-var arrow = document.querySelector(".arrow");
-var github = document.querySelector(".github-icon");
-var linkedin = document.querySelector(".linkedin-icon");
-var linkedin2 = document.querySelector(".linkedin-icon-2");
-var experience = document.querySelector(".experience-icon");
-var education = document.querySelector(".education-icon")
-var email = document.querySelector(".email-icon");
+const viewIcons = [document.getElementById("view-icon"), document.getElementById("view-icon-1")];
+const arrow = document.querySelector(".arrow");
+const icons = {
+    github: document.querySelector(".github-icon"),
+    linkedin: document.querySelector(".linkedin-icon"),
+    linkedin2: document.querySelector(".linkedin-icon-2"),
+    experience: document.querySelector(".experience-icon"),
+    education: document.querySelector(".education-icon"),
+    email: document.querySelector(".email-icon")
+};
 
-viewIcon.onclick = function () {
+function toggleTheme(icon) {
     document.body.classList.toggle("dark-theme");
-    if (document.body.classList.contains("dark-theme")) {
-        viewIcon.setAttribute("src", "./assets/sun.png");
-        arrow.setAttribute("src", "./assets/arrow-white.png");
-        github.setAttribute("src", "./assets/github-white.png");
-        experience.setAttribute("src", "./assets/experience-white.png");
-        education.setAttribute("src", "./assets/education-white.png");
-        linkedin.setAttribute("src", "./assets/linkedin-white.png");
-        linkedin2.setAttribute("src", "./assets/linkedin-white.png");
-        email.setAttribute("src", "./assets/email-white.png");
-    } else {
-        viewIcon.setAttribute("src", "./assets/moon.png");
-        arrow.setAttribute("src", "./assets/arrow.png");
-        github.setAttribute("src", "./assets/github.png");
-        experience.setAttribute("src", "./assets/experience.png");
-        education.setAttribute("src", "./assets/education.png");
-        linkedin.setAttribute("src", "./assets/linkedin.png");
-        linkedin.setAttribute("src", "./assets/linkedin.png");
-        email.setAttribute("src", "./assets/email.png");
-    }
+    const isDarkTheme = document.body.classList.contains("dark-theme");
+
+    icon.setAttribute("src", isDarkTheme ? "./assets/sun.png" : "./assets/moon.png");
+    arrow.setAttribute("src", isDarkTheme ? "./assets/arrow-white.png" : "./assets/arrow.png");
+
+    icons.github.setAttribute("src", isDarkTheme ? "./assets/github-white.png" : "./assets/github.png");
+    icons.experience.setAttribute("src", isDarkTheme ? "./assets/experience-white.png" : "./assets/experience.png");
+    icons.education.setAttribute("src", isDarkTheme ? "./assets/education-white.png" : "./assets/education.png");
+    icons.linkedin.setAttribute("src", isDarkTheme ? "./assets/linkedin-white.png" : "./assets/linkedin.png");
+    icons.linkedin2.setAttribute("src", isDarkTheme ? "./assets/linkedin-white.png" : "./assets/linkedin.png");
+    icons.email.setAttribute("src", isDarkTheme ? "./assets/email-white.png" : "./assets/email.png");
 }
 
-viewIcon1.onclick = function () {
-    document.body.classList.toggle("dark-theme");
-    if (document.body.classList.contains("dark-theme")) {
-        viewIcon1.setAttribute("src", "./assets/sun.png");
-        arrow.setAttribute("src", "./assets/arrow-white.png");
-        github.setAttribute("src", "./assets/github-white.png");
-        experience.setAttribute("src", "./assets/experience-white.png");
-        education.setAttribute("src", "./assets/education-white.png");
-        linkedin.setAttribute("src", "./assets/linkedin-white.png");
-        linkedin2.setAttribute("src", "./assets/linkedin-white.png");
-        email.setAttribute("src", "./assets/email-white.png");
-    } else {
-        viewIcon1.setAttribute("src", "./assets/moon.png");
-        arrow.setAttribute("src", "./assets/arrow.png");
-        github.setAttribute("src", "./assets/github.png");
-        experience.setAttribute("src", "./assets/experience.png");
-        education.setAttribute("src", "./assets/education.png");
-        linkedin.setAttribute("src", "./assets/linkedin.png");
-        linkedin.setAttribute("src", "./assets/linkedin.png");
-        email.setAttribute("src", "./assets/email.png");
-    }
-}
+viewIcons.forEach(icon => {
+    icon.onclick = function () {
+        toggleTheme(icon);
+    };
+});
