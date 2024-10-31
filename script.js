7fae6f2481411f7f2cf95843e03547e71f91dbab
Kamesh Kumar <kameshkumar4602@gmail.com>
2024-10-31 23:21:59 +0530

    feature: Implemented the dark mode icons to be white

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toggleMenu(){
+function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
@@ -7,26 +7,42 @@ function toggleMenu(){
 
 var viewIcon = document.getElementById("view-icon");
 var viewIcon1 = document.getElementById("view-icon-1");
+var arrow = document.querySelector(".arrow");
+var github = document.querySelector(".github-icon");
+var linkedin = document.querySelector(".linkedin-icon");
+var linkedin2 = document.querySelector(".linkedin-icon-2");
+var experience = document.querySelector(".experience-icon");
+var education = document.querySelector(".education-icon")
+var email = document.querySelector(".email-icon");
 
-viewIcon.onclick = function(){
-    console.log("happy")
+viewIcon.onclick = function () {
     document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
+    if (document.body.classList.contains("dark-theme")) {
         viewIcon.setAttribute("src", "./assets/sun.png");
-    }else{
+        arrow.setAttribute("src", "./assets/arrow-white.png");
+        github.setAttribute("src", "./assets/github-white.png");
+        experience.setAttribute("src", "./assets/experience-white.png");
+        education.setAttribute("src", "./assets/education-white.png");
+        linkedin.setAttribute("src", "./assets/linkedin-white.png");
+        linkedin2.setAttribute("src", "./assets/linkedin-white.png");
+        email.setAttribute("src", "./assets/email-white.png");
+    } else {
         viewIcon.setAttribute("src", "./assets/moon.png");
+        arrow.setAttribute("src", "./assets/arrow.png");
+        github.setAttribute("src", "./assets/github.png");
+        experience.setAttribute("src", "./assets/experience.png");
+        education.setAttribute("src", "./assets/education.png");
+        linkedin.setAttribute("src", "./assets/linkedin.png");
+        linkedin.setAttribute("src", "./assets/linkedin.png");
+        email.setAttribute("src", "./assets/email.png");
     }
 }
 
-viewIcon1.onclick = function(){
-    console.log("happy")
+viewIcon1.onclick = function () {
     document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
+    if (document.body.classList.contains("dark-theme")) {
         viewIcon1.setAttribute("src", "./assets/sun.png");
-    }else{
+    } else {
         viewIcon1.setAttribute("src", "./assets/moon.png");
     }
 }
-
-
- 
